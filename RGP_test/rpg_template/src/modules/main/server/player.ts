468309bbc8b9cc339b1697daefe974d02750c499
avulic <antevulic96@gmail.com>
2023-07-25 17:04:08 +0200

    Deleted RGP_test

--- RGP_test/rpg_template/src/modules/main/server/player.ts
+++
@@ -1,9 +0,0 @@
-import { RpgPlayer, RpgPlayerHooks, Control } from '@rpgjs/server'
-
-export const player: RpgPlayerHooks = {
-    onInput(player: RpgPlayer, { input }) {
-        if (input == Control.Back) {
-            player.callMainMenu()
-        }
-    }
-}
