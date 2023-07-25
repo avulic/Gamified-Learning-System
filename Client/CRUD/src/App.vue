5372de312d1fe03edbab411a0245788e6bf27d6c
avulic <antevulic96@gmail.com>
2023-07-25 10:48:55 +0200

    first commit

---
+++ Client/CRUD/src/App.vue
@@ -0,0 +1,102 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <RouterLink to="/" class="navbar-brand">bezKoder</RouterLink>
+
+        <div class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <RouterLink to="/tutorials" class="nav-link">Tutorials</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink to="/add" class="nav-link">Add</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink to="/game" class="nav-link">Game</RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink to="/about" class="nav-link">About</RouterLink>
+          </li>
+        </div>
+      </nav>
+    </div>
+  </header>
+  
+  <div class="container mt-3">
+    <RouterView />
+  </div>
+
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  max-height: 100vh;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+
+  nav {
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+
+    padding: 1rem 0;
+    margin-top: 1rem;
+  }
+}
+</style>
---
+++ RGP_test/rpg_template/src/modules/index.ts
@@ -0,0 +1,13 @@
+import main from './main'
+import starterKit from './starter'
+import defaultGui from '@rpgjs/default-gui'
+import mobileGui from '@rpgjs/mobile-gui'
+import gamepad from '@rpgjs/gamepad'
+
+export default [
+    main,
+    starterKit,
+    defaultGui,
+    mobileGui,
+    gamepad
+]
